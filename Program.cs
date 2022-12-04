04b182577ba38bfe05f4f9f783431c8ff3c2fe76
mic <mic_st@mail.ru>
2022-12-04 18:08:25 +0400

    create program creation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿string[] firstArray = new string[] { "hello", "2", "World", ":-)", "1234", "GeekBrains", "edu" };
+int n = 3;
+
+int secondLenght = GetNumberSample(firstArray, n);
+
+string[] resultArray = GetResult(firstArray, n, secondLenght);
+
+Console.WriteLine($"[{string.Join(", ", firstArray)}] -> [{string.Join(", ", resultArray)}]");
+
+
+//--------
+
+int GetNumberSample(string[] firstArray, int lenghtString)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= lenghtString)
+            count++;
+    }
+    return count;
+}
+
+
+string[] GetResult(string[] firstArray, int lenghtString, int secondLenght)
+{
+    string[] resultArr = new string[secondLenght];
+    int i = 0;
+    for (int j = 0; j < firstArray.Length; j++)
+    {
+        if (firstArray[j].Length <= lenghtString)
+        {
+            resultArr[i] = firstArray[j];
+            i++;
+        }
+    }
+    return resultArr;
+}
